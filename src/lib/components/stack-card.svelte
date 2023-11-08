3c8a2681a87a7d5acbc0c675590542a3a339c0a7
Jake Jasmin <roastbeefer000@gmail.com>
2023-11-07 19:18:02 -0700

    stuff

---
+++ src/lib/components/stack-card.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let title;
+</script>
+
+<div class="rounded bg-red p-2 m-2">
+	<h2 class="text-surface0 font-bold underline">{title}</h2>
+	<p>Write a meaningful description here.</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script>
+    import StackCard from "../lib/components/stack-card.svelte";
 </script>
 
-<div>
-    <h1 class="text-4xl">Hi, I'm Jake Jasmin</h1>
-    <p class="text-black m-2 p-2">Text</p>
-    <p>
-        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-    </p>
+<div class="justify-center text-center">
+    <h1 class="text-4xl text-blue m-4">Hi, I'm Jake Jasmin</h1>
+</div>
+
+<div class="justify-center text-center">
+    <h1 class="text-text">My tech stack:</h1>
+</div>
+<div class="justify-center mx-auto p-4 grid grid-cols-3 w-3/4">
+    <StackCard title="Rust" />
+    <StackCard title="Svelte" />
+    <StackCard title="Terraform" />
+    <StackCard title="Python" />
+    <StackCard title="Stuff" />
+    <StackCard title="Other things" />
 </div>
