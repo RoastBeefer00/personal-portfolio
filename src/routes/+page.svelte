2abb20178f8d7a997cdfe70e3b4fdc3911b667cf
Jake Jasmin <roastbeefer000@gmail.com>
2023-11-07 18:45:20 -0700

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div>
+    <h1 class="text-4xl">Hi, I'm Jake Jasmin</h1>
+    <p class="text-black m-2 p-2">Text</p>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+</div>
