a7bc13b0c519e829d08917f3192650cf365199e6
Jake Jasmin <roastbeefer000@gmail.com>
2023-11-15 19:53:07 -0700

    add linux and ccc projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,117 @@
 <script>
 </script>
 
-<div class="w-3/4 sm:w-1/2 bg-blue justify-center mx-auto m-4 rounded">
-	<h1 class="text-crust text-4xl sm:text-8xl text-center p-8">
+<div class="w-3/4 sm:w-1/2 bg-blue justify-center mx-auto m-4 rounded p-4">
+	<h1 class="text-crust text-4xl sm:text-6xl text-center">
 		<b>Projects</b>
 	</h1>
+	<p class="text-crust text-center m-2">
+		Check out my <a
+			href="https://github.com/RoastBeefer00?tab=repositories"
+			class="underline">Github</a
+		>!
+	</p>
+</div>
+
+<div class="p-4">
+	<div class="bg-blue rounded p-4 mb-2">
+		<div class="rounded bg-surface0 p-2 mb-2 text-center">
+			<h1 class="text-3xl text-text font-bold">Linux Rice</h1>
+		</div>
+		<div class="sm:flex">
+			<div class="sm:flex-auto">
+				<img src="desktop.png" class="object-scale-down" />
+			</div>
+			<div class="sm:flex-auto ml-2 mb-2">
+				<div class="rounded bg-surface1 p-2 mb-2">
+					<h2 class="text-xl font-bold text-text">EndeavorOS</h2>
+					<p class="text-crust bg-overlay2 p-2 rounded">
+						<a href="https://endeavouros.com/" class="underline"
+							>EndeavorOS</a
+						> is a Linux distribution based on Arch. It makes Arch installation
+						a little easier than vanilla Arch and comes with a few handy
+						applications pre-loaded. The Arch User Repository (AUR) and
+						package manager make it extremely simple to install any you
+						might need.
+					</p>
+				</div>
+				<div class="rounded bg-surface1 p-2 mb-2">
+					<h2 class="text-xl font-bold text-text">Hyprland</h2>
+					<p class="text-crust bg-overlay2 p-2 rounded">
+						Here's my <a
+							href="https://github.com/RoastBeefer00/hyprland"
+							class="underline">Hyprland config</a
+						>. There's simply no going back after getting used to a
+						tiling window manager. Knowing how to navigate your
+						operating system is crucial. With this setup I never
+						wonder where my terminal(s) or browser are. Everything
+						is at my finger tips within a few keypresses away.
+					</p>
+				</div>
+				<div class="rounded bg-surface1 p-2 mb-2">
+					<h2 class="text-xl font-bold text-text">Tmux</h2>
+					<p class="text-crust bg-overlay2 p-2 rounded">
+						<a
+							href="https://github.com/tmux/tmux/wiki"
+							class="underline">Tmux</a
+						>
+						is a terminal multiplexer that allows me to quickly and easily
+						switch between projects. My
+						<a
+							href="https://github.com/RoastBeefer00/tmux-conf"
+							class="underline"
+							>config
+						</a>contains some useful plugins that customize the way
+						it looks and restores my sessions so I can pick back up
+						right where I left off.
+					</p>
+				</div>
+				<div class="rounded bg-surface1 p-2">
+					<h2 class="text-xl font-bold text-text">Helix</h2>
+					<p class="text-crust bg-overlay2 p-2 rounded">
+						Helix doesn't require much <a
+							href="https://github.com/RoastBeefer00/helix-config"
+							class="underline">configuration</a
+						>, however I do like to change some keybinds to match
+						what I found to be more intuitive in Neovim.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="bg-red rounded p-4 mb-2">
+		<div class="rounded bg-surface0 p-2 mb-2 text-center">
+			<h1 class="text-3xl text-text font-bold">Cruces Chess Club</h1>
+		</div>
+		<div class="sm:flex">
+			<div class="sm:flex-none mr-2 mb-2 sm:w-1/3">
+				<div class="rounded bg-surface1 p-2 mb-2">
+					<h2 class="text-xl text-text font-bold">CCC</h2>
+					<p class="text-crust bg-overlay2 p-2 rounded">
+						As a member of the board for my local chess club I took
+						it upon myself to build a <a
+							href="https://www.cruceschessclub.org/"
+							class="underline"
+							>website
+						</a>
+						so anyone can easily see where we'll be each week. The frontend
+						is built using Svelte and the Bulma CSS framework. If I could
+						go back I'd use TailwindCSS because that is what I'm more
+						comfortable with now and I find it to be far more customizable.
+						I learned a lot on this project and it's the first time I
+						ever registered a domain!
+						<br />
+						Check out my
+						<a
+							href="https://github.com/RoastBeefer00/cruceschessclub"
+							class="underline">source code</a
+						>!
+					</p>
+				</div>
+			</div>
+			<div class="sm:flex-auto">
+				<img src="ccc.png" class="object-scale-down" />
+			</div>
+		</div>
+	</div>
 </div>
